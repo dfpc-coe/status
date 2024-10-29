fc73b557491857a0603aea62634a46639a3d8fc8
ingalls <nick@ingalls.ca>
2024-10-29 16:06:36 -0600

    Remove Local Requirement

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig((configEnv) => ({
-    base: process.env.LOCAL ? '/' : '/status/',
     plugins: [
         vue(),
     ],
