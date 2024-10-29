92503659008960e6364504e3bdf3153b094361fe
ingalls <nick@ingalls.ca>
2024-10-29 11:37:47 -0600

    Update Dates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,24 +9,34 @@
             <IconCircleCheck :size='64' stoke='1' color='green'/>
         </div>
         <div class='col-12 d-flex align-items-center justify-content-center'>
-            <h1>All services are online</h1>
+            <h1 class='user-select-none'>All services are online</h1>
         </div>
         <div class='col-12 d-flex align-items-center justify-content-center'>
-            <span class='subheader'>As of October 29, 5:45 PM CET</span>
+            <span class='subheader user-select-none'>As of <span v-text='requestDate'/></span>
         </div>
     </div>
 
-    <div class='container-xl pt-4'>
+    <div class='container pt-4'>
         <div class='card'>
             <div class='card-header'>
                 <div class='card-title'>Services</div>
+                <div class='ms-auto'>
+                    <TablerIconButton
+                        title='Refresh'
+                    >
+                        <IconRefresh :size='32' stroke='1'/>
+                    </TablerIconButton>
+                </div>
             </div>
             <div class='card-body'>
                 <template v-for='service in services'>
                     <div class='col-12 pb-2'>
                         <div class='d-flex align-items-center'>
                             <div class='d-flex align-items-center'>
-                                <span class="status-indicator status-green status-indicator-animated">
+                                <span
+                                    v-tooltip='"Current Status"'
+                                    class="status-indicator status-green status-indicator-animated"
+                                >
                                     <span class="status-indicator-circle"></span>
                                     <span class="status-indicator-circle"></span>
                                     <span class="status-indicator-circle"></span>
@@ -37,8 +47,8 @@
                             <div class='ms-auto d-flex'>
                                 <div
                                     v-for='date in dates'
-                                    v-tooltip='date'
-                                    class='date bg-green rounded'
+                                    v-tooltip='`${date.toLocaleString("default", { month: "long" })} ${date.getUTCDate()}`'
+                                    class='date bg-green rounded cursor-pointer'
                                     style='
                                         width: 12px;
                                         height: 32px;
@@ -64,6 +74,10 @@
 <script setup lang='ts'>
 import { ref, computed } from 'vue';
 import {
+    TablerIconButton
+} from '@tak-ps/vue-tabler';
+import {
+    IconRefresh,
     IconCircleCheck
 } from '@tabler/icons-vue';
 
@@ -76,13 +90,19 @@ const dates = computed(() => {
         dates.push(date);
     }
 
-    return dates;
+    return dates.reverse();
 });
+
+const requestDate = ref(new Date());
 
 const services = ref([{
     name: 'COTAK.gov'
 },{
-    name: 'TAK Server'
+    name: 'TAK Serer'
+},{
+    name: 'Authentication Service'
+},{
+    name: 'CloudTAK Service'
 }]);
 
 </script>
